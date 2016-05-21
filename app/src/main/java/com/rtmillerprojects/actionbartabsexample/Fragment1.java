2adaffbc8cb59d0680c63a44fc7586faf7a6fa03
Ryan <rmiller07@gmail.com>
2016-05-21 15:48:47 -0400

    init for tab example

---
+++ app/src/main/java/com/rtmillerprojects/actionbartabsexample/Fragment1.java
@@ -0,0 +1,16 @@
+package com.rtmillerprojects.actionbartabsexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Ryan on 5/21/2016.
+ */
+public class Fragment1 extends Fragment {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstance){
+        
+    }
+}
